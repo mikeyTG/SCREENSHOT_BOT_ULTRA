2a906464ba71bcf82bb879606f78ebe606e26298
mikeyTG <88639357+mikeyTG@users.noreply.github.com>
2021-08-10 08:31:06 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -8,18 +8,18 @@ from ..screenshotbot import ScreenShotBot
 async def start(c, m):
 
     await m.reply_text(
-        text=f"Hi there {m.from_user.mention}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from "
+        text=f"Hi BRO {m.from_user.mention}.\n\nI'm Screenshot Bot PRO😋. I can provide screenshots from "
         "your video files without downloading the entire file (almost instantly). For more details check /help.",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Source 😒", url="https://github.com/odysseusmax/animated-lamp"
+                        "MY DP😒", url="https://telegra.ph/file/a49001f8ed1caaa8ce2fd.jpg"
                     ),
-                    InlineKeyboardButton("Project Channel", url="https://t.me/odbots"),
+                    InlineKeyboardButton("Project Channel", url="https://t.me/MRGBOTREPAIR"),
                 ],
-                [InlineKeyboardButton("My Father", url="https://t.me/odysseusmax")],
+                [InlineKeyboardButton("My Father", url="https://t.me/@YOUTUBERYT54355")],
             ]
         ),
     )
